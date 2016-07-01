ace3e625087e60d13da699b72725bd2c8af1fd22
frvpanlilio <frvpanlilio@gmail.com>
2016-07-01 14:38:51 +0800

    Update memory_check.sh

--- memory_check.sh
+++ memory_check.sh
@@ -4,6 +4,12 @@ clear
 MEMORY_USAGE=$(free -m | grep Mem: | awk '{printf("%.0f"),$3/$2 * 100.0}')
 echo "TOTAL MEMORY USAGE $MEMORY_USAGE%";
 
+TOP_PROCESESS=$( ps aux --sort -rss | head -n 11 )
+echo "$TOP_PROCESESS"
+
+EMAIL_SUBJECT=`date +%Y%m%d" "%H:%M`
+echo "$EMAIL_SUBJECT"
+
 if [ $# -eq 0 ]; then
 echo "Please Input Valid Parameters"
 exit 0
@@ -18,7 +24,7 @@ while getopts ":c:w:e:" opt; do
         e)
           email=$OPTARG;;
         :)
-          echo "Invalid Parameters, please supply -w 60 -c 90 -e email@me.com"
+          echo "Invalid Parameters, please supply Ex. memory_check -w 60 -c 90 -e email@me.com"
           exit 0
           ;;
     esac
@@ -31,11 +37,11 @@ if [ $warning -lt $critical ]; then
                 exit 0
         elif [[ $MEMORY_USAGE -ge $warning && $MEMORY_USAGE -lt $critical ]]; then
                 echo "The current memory is in warning"
-                echo "The current memory is in warning" | mail -s "MEMORY AY WARNING NA" frvpanlilio@chikka.com
+                echo "The current memory is in warning" | mail -s "$EMAIL_SUBJECT memory check-warning" $email
                 exit 1
         elif [ $MEMORY_USAGE -ge $critical ]; then
                 echo "The current memory is in critical"
-                echo "The current memory is in critical" | mail -s "MEMORY AY CRITICAL NA" frvpanlilio@chikka.com
+                echo -e "The current memory is in critical \n \n $TOP_PROCESESS" | mail -s "$EMAIL_SUBJECT memory check-critical" $email
                 exit 2
         fi
 else
